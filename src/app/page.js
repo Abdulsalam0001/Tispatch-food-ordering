ec2184bc9b92c5a25be26286e332cfa250e97853
chris chase <dreshaw2012@gmail.com>
2024-12-04 13:39:44 +0000

    savw

--- src/app/page.js
+++ src/app/page.js
@@ -10,20 +10,25 @@ export default function Home() {
       <HomeMenu />
       <section className="text-center my-16" id="about">
         <SectionHeaders
-          subHeader={'Our story'}
-          mainHeader={'About us'}
+          subHeader={'NO.1 Food Delivery Service'}
+          mainHeader={'About Tispatch'}
         />
         <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni minima odit recusandae. Illum ipsa non repudiandae? Eum ipsam iste quos suscipit tempora? Aperiam esse fugiat inventore laboriosam officiis quam rem!
-          </p>
-          <p>At consectetur delectus ducimus est facere iure molestias obcaecati quaerat vitae voluptate? Aspernatur dolor explicabo iste minus molestiae pariatur provident quibusdam saepe?</p>
-          <p>Laborum molestias neque nulla obcaecati odio quia quod reprehenderit sit vitae voluptates? Eos, tenetur.</p>
+          Get Fresh, Delicious Food
+          Delivered to Your Door          </p>
+          <p>Tispatch brings you an array of delicious meals from your favorite local restaurants, delivered fast and fresh right to you.</p>
+          <p>Tispatch is on a mission to make food delivery fast, convenient, and affordable. Whether you’re craving a healthy meal, comfort food, or a quick snack, Tispatch has you covered.
+
+Wide selection of restaurants and cuisines
+Real-time order tracking from start to finish
+Fast and reliable delivery with easy app navigation
+.</p>
         </div>
       </section>
       <section className="text-center my-8" id="contact">
         <SectionHeaders
-          subHeader={'Don\'t hesitate'}
+          subHeader={'Need help with your order?'}
           mainHeader={'Contact us'}
         />
         <div className="mt-8">
